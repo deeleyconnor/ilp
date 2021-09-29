09d6995cf36d6854674d191c289c47dd3742afd4
Connor Deeley <deeleyconnor@c142134.wlan.net.ed.ac.uk>
2021-09-29 21:49:58 +0100

    Created LongLat Class

---
+++ src/main/java/uk/ac/ed/inf/LongLat.java
@@ -0,0 +1,12 @@
+package uk.ac.ed.inf;
+
+public class LongLat {
+
+    public double longitude;
+    public double latitude;
+
+    public LongLat (double longitude, double latitude)
+    {
+        
+    }
+}
