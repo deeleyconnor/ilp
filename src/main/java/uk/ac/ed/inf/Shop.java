fb4a17ebf8efafdd7d1425e87c9ef1739be65f9c
Connor Deeley <deeleyconnor@Connors-MacBook-Pro.local>
2021-10-01 13:03:29 +0100

    Created Shop Class

--- src/main/java/uk/ac/ed/inf/Shop.java
+++ src/main/java/uk/ac/ed/inf/Shop.java
@@ -1,2 +1,12 @@
-package uk.ac.ed.inf;public class Shop {
+package uk.ac.ed.inf;
+
+public class Shop {
+    String name;
+    String location;
+
+    Item[] menu;
+    public static class Item {
+        String item;
+        int pence;
+    }
 }
