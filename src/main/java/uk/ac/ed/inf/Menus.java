522133ba456fd73897ae116c140c75780d102f01
Connor Deeley <deeleyconnor@Connors-MacBook-Pro.local>
2021-10-01 10:17:37 +0100

    Created Menus Constructor

--- src/main/java/uk/ac/ed/inf/Menus.java
+++ src/main/java/uk/ac/ed/inf/Menus.java
@@ -1,4 +1,16 @@
 package uk.ac.ed.inf;
 
 public class Menus {
+
+    public String machineName;
+    public String port;
+
+    public Menus(String machineName, String port){
+        this.machineName = machineName;
+        this.port = port;
+    }
+
+    public int getDeliveryCost(String ... order) {
+        return 50;
+    }
 }
