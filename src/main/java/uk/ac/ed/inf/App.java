b3430522050586921e0eb8d97de994006c8007ff
Connor Deeley <deeleyconnor@Connors-MacBook-Pro.local>
2021-10-06 17:43:27 +0100

    Removed HttpRequests from Menus into App due to this being used by multiple classes later to avoid repeated code

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,13 +1,45 @@
 package uk.ac.ed.inf;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static final HttpClient httpClient = HttpClient.newHttpClient();
+
+    public static void main(String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "System Started!" );
+    }
+
+    public static String websiteRequest(String urlString) {
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlString)).build();
+
+        try {
+            HttpResponse <String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                return response.body();
+            }
+            else {
+                System.out.println("Menu Request Failed");
+                System.out.println(response.statusCode());
+                System.exit(1);
+            }
+
+        }
+        catch (Exception e) {
+            System.out.println("Menus Request Failed");
+            System.out.println(e);
+            System.exit(1);
+        }
+
+        return "";
     }
 }
--- src/main/java/uk/ac/ed/inf/Menus.java
+++ src/main/java/uk/ac/ed/inf/Menus.java
@@ -2,45 +2,18 @@ package uk.ac.ed.inf;
 
 import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.net.URI;
-
-
 public class Menus {
 
     private final static int STANDARD_DELIVERY_CHARGE = 50;
-
-    public static final HttpClient client = HttpClient.newHttpClient();
+    private final static String MENUS_FILE_LOCATION = "menus/menus.json";
 
     private Shop[] shops;
 
     public Menus(String machineName, String port) {
-        String urlString = String.format("http://%s:%s/menus/menus.json", machineName, port); ;
+        String urlString = String.format("http://%s:%s/%s", machineName, port, MENUS_FILE_LOCATION); ;
+        String responseBody = App.websiteRequest(urlString);
 
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlString)) .build();
-
-        try {
-            HttpResponse <String> response = client.send(request, BodyHandlers.ofString());
-
-            if (response.statusCode() == 200) {
-                shops = new Gson().fromJson(response.body(), Shop[].class);
-            }
-            else {
-                System.out.println("Menu Request Failed");
-                System.out.println(response.statusCode());
-                System.exit(1);
-            }
-
-        }
-        catch (Exception e) {
-            System.out.println("Menus Request Failed");
-            System.out.println(e);
-            System.exit(1);
-        }
+        shops = new Gson().fromJson(responseBody, Shop[].class);
     }
 
     public int getDeliveryCost(String ... order) {
