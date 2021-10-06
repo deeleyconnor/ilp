b99e34f5ade505c7bab5fb11010e0b0f8c87ec2b
Connor Deeley <deeleyconnor@Connors-MacBook-Pro.local>
2021-10-06 17:41:48 +0100

    Added JavaDoc Comments to Shop Class

--- src/main/java/uk/ac/ed/inf/Shop.java
+++ src/main/java/uk/ac/ed/inf/Shop.java
@@ -1,10 +1,19 @@
 package uk.ac.ed.inf;
 
+/**
+ * Represents a shop participating in the drone lunch delivery service.
+ * Contains the name, location of the shop as well as the items that it sells.
+ */
 public class Shop {
     String name;
     String location;
 
     Item[] menu;
+
+    /**
+     * Represents an item sold by the shop.
+     * Contains the name of the item as well as the price in pence.
+     */
     public static class Item {
         String item;
         int pence;
