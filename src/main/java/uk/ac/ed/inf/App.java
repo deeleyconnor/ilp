007c520055219f58b8a63a2a6962337b1661919d
Connor Deeley <deeleyconnor@Connors-MacBook-Pro.local>
2021-10-19 23:43:15 +0100

    Started App Implementation

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,17 +1,30 @@
 package uk.ac.ed.inf;
 
+import java.sql.SQLException;
+
 /**
  * This class is the main app.
  */
 public class App 
 {
+
+    private static final String MACHINE_NAME = "localhost";
+
     /**
-     * Main method of the app class.
      *
-     * @param args Arguments given on initialisation
+     * @param day
+     * @param month
+     * @param year
+     * @param webserverPort
+     * @param databasePort
+     * @throws SQLException
      */
-    public static void main(String[] args )
-    {
+    public static void main(String day, String month, String year,String webserverPort, String databasePort) throws SQLException {
         System.out.println( "System Started!" );
+
+        DatabaseClient databaseClient = new DatabaseClient(MACHINE_NAME, databasePort);
+
+
     }
+
 }
