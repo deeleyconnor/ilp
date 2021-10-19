77bd4d27e939c6961e68a5b53619fd67d2b5f9a2
Connor Deeley <deeleyconnor@Connors-MacBook-Pro.local>
2021-10-19 23:42:46 +0100

    Implemented Database OrderRetrieval

--- src/main/java/uk/ac/ed/inf/DatabaseClient.java
+++ src/main/java/uk/ac/ed/inf/DatabaseClient.java
@@ -1,2 +1,54 @@
-package uk.ac.ed.inf;public class DatabaseClient {
+package uk.ac.ed.inf;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DatabaseClient {
+
+    private static final String DATABASE = "derbyDB";
+    private static final String ORDERS_QUERY = "select * from orders where deliveryData=(?)";
+    private static final String ORDER_DETAILS_QUERY = "select * from orderDetails where orderNo=(?)";
+
+    private final Connection conn;
+    private final PreparedStatement psOrdersQuery;
+    private final PreparedStatement psOrderDetailsQuery;
+
+    //PLease Handle SQL Exception
+    public DatabaseClient(String machineName, String port) throws SQLException {
+        String jdbcString = String.format("jdbc:derby://%s:%s/%s", machineName, port, DATABASE);
+
+        conn = DriverManager.getConnection(jdbcString);
+
+        psOrdersQuery = conn.prepareStatement(ORDERS_QUERY);
+        psOrderDetailsQuery = conn.prepareStatement(ORDER_DETAILS_QUERY);
+    }
+
+    public ArrayList<Order> getOrders(String date) throws SQLException {
+        psOrdersQuery.setString(2,date);
+
+        ArrayList<Order> orders = new ArrayList<>();
+        ResultSet rs = psOrdersQuery.executeQuery();
+        while (rs.next()) {
+            String orderNo = rs.getString("orderNo");
+            String customer = rs.getString("customer");
+            String words = rs.getString("deliverTo");
+            ArrayList<String> items = getOrderDetails(orderNo);
+            orders.add(new Order(orderNo, customer, items, words));
+        }
+
+        return orders;
+    }
+
+    private ArrayList<String> getOrderDetails(String orderNo) throws SQLException {
+        psOrderDetailsQuery.setString(1,orderNo);
+
+        ArrayList<String> items = new ArrayList<>();
+        ResultSet rs = psOrderDetailsQuery.executeQuery();
+        while (rs.next()) {
+            String item = rs.getString("item");
+            items.add(item);
+        }
+
+        return items;
+    }
 }
