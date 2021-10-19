7443b0c8b1907aa83bdcae79e1c05ea7c7c38d20
Connor Deeley <deeleyconnor@Connors-MacBook-Pro.local>
2021-10-19 23:13:37 +0100

    Created Order Class

--- src/main/java/uk/ac/ed/inf/Order.java
+++ src/main/java/uk/ac/ed/inf/Order.java
@@ -1,2 +1,19 @@
-package uk.ac.ed.inf;public class Order {
+package uk.ac.ed.inf;
+
+import java.util.ArrayList;
+
+public class Order {
+
+    private String orderNo;
+    private String customer;
+    private LongLat deliverTo;
+    private ArrayList<String> order;
+
+    public Order(String orderNo, String customer,ArrayList<String> order,  String words) {
+        this.orderNo = orderNo;
+        this.customer = customer;
+        this.order = order;
+
+        //Figure way to convery words to LongLat
+    }
 }
